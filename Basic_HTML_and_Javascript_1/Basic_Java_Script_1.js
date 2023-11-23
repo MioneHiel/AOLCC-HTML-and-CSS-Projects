52569dadc9664dfb92b9d2a79fcfb8d6d032a4d5
Aga Palaganas <149645611+MioneHiel@users.noreply.github.com>
2023-11-22 21:34:10 -0700

    Add files via upload

---
+++ Basic_HTML_and_Javascript_1/Basic_Java_Script_1.js
@@ -0,0 +1,8 @@
+
+alert("I am an alert box!");
+
+function My_First_Function() {
+		var String = "Kiss me, I am Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML = result;
+		}
